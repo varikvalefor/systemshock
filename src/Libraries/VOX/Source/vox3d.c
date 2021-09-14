2912e1f79e5ecb5ef4fb02366c972ec342f44b91
Varik Valefor <varikvalefor@aol.com>
2021-09-14 18:33:37 -0400

    Libraries/VOX/Source/vox3d.c: Some additional commented stuff is thrown in the gutter.

--- src/Libraries/VOX/Source/vox3d.c
+++ src/Libraries/VOX/Source/vox3d.c
@@ -79,7 +79,6 @@ void vmap_dot(fix x0, fix y0, fix dxdu, fix dydu, fix dxdv, fix dydv, fix dxdz, 
 void vx_render(vxs_vox *vx)
 {
    fix a,b;
-   //int f;
    int near_ver;   //which vertex is nearest
    g3s_vector p[4];
    g3s_phandle tmp[4];
@@ -120,13 +119,6 @@ void vx_render(vxs_vox *vx)
       return;
    }
 
-   // find max and min x and y
-   // note that maxdx and maxdy are in fix
-   //mprintf("tmp[0]->sx,sy = %x %x\n",tmp[0]->sx,tmp[0]->sy);
-  // mprintf("tmp[1]->sx,sy = %x %x\n",tmp[1]->sx,tmp[1]->sy);
-   //mprintf("tmp[2]->sx,sy = %x %x\n",tmp[2]->sx,tmp[2]->sy);
-   //mprintf("tmp[3]->sx,sy = %x %x\n",tmp[3]->sx,tmp[3]->sy);
-
    // turn vectors into deltas
    for (i=1;i<4;++i) {
       tmp[i]->sx -= tmp[0]->sx;
@@ -151,15 +143,12 @@ void vx_render(vxs_vox *vx)
    psy = fix_div(grd_bm.w * vx->pix_size,tmp[0]->gZ)>>1;
    psx = fix_mul(psy,grd_cap->aspect);
 
-//   mprintf("psx = %x psy = %x\n",psx,psy);
-
    // make them at least one pixel
    if (psy<FIX_UNIT) psy = FIX_UNIT;
    if (psx<FIX_UNIT) psx = FIX_UNIT;
 
    maxdx = (psx<<1) + (vx->w*fix_abs(tmp[1]->sx) + vx->h*fix_abs(tmp[2]->sx) + vx->d*fix_abs(tmp[3]->sx));
    maxdy = (psy<<1) + (vx->w*fix_abs(tmp[1]->sy) + vx->h*fix_abs(tmp[2]->sy) + vx->d*fix_abs(tmp[3]->sy));
-   //mprintf("maxdx = %x maxdy = %x\n",maxdx,maxdy);
 
    // these can overflow and become negative in extreme situations causing it not to clip
    if ((maxdy < 0) || (maxdx < 0)) {
@@ -223,20 +212,6 @@ void vx_render(vxs_vox *vx)
    gr_fix_line(x[3],y[3],x[0],y[0]);
 #endif
 
-   //mprintf("f = %d clip = %d\n",f++,clip);
-
-   //   if (f==97)
-   //      mprintf("uh oh\n");
-
-   //   x[0] = tmp[0]->sx + maxdx;
-   //   x[1] = tmp[0]->sx - maxdx;
-   //   y[0] = tmp[0]->sy + maxdy;
-   //   y[1] = tmp[0]->sy - maxdy;
-   //   gr_fix_line(x[0],y[0],x[1],y[0]);
-   //   gr_fix_line(x[1],y[0],x[1],y[1]);
-   //   gr_fix_line(x[1],y[1],x[0],y[1]);
-   //   gr_fix_line(x[0],y[1],x[0],y[0]);
-
    // find near vertex
    near_ver = 0;
    min_z = tmp[0]->gZ;
@@ -254,29 +229,6 @@ void vx_render(vxs_vox *vx)
       near_ver = 3;
       min_z = tmp[2]->gZ;
    }
-
-   //mprintf("near_ver = %d\n",near_ver);
-
-   // calculate face 1
-   //   for(i=0;i<1;++i) {
-   //      // defines which faces visible at that vertex
-   //      f = corners[near_ver][i];
-   //      f = 0;
-   //      for(j=0;j<4;++j) {
-   //         // faces gives vertices at each face
-   //         x[j] = a+v[faces[f][j]] [0];
-   //         y[j] = b+v[faces[f][j]] [1];
-   //      }
-
-   // find out which one of the vertices is the near_ver
-   // faces looks for all the vertices there
-   //for(j=0;j<4;++j) {
-   //   if (near_ver == faces[f][j]) break;
-   //}
-   // f= 0;
-
-   //mprintf("clip = %d\n",clip);
-   //mprintf("tmp[0]->gZ = %x\n",tmp[0]->gZ);
 
    vmap_dot(a,b,tmp[1]->sx,tmp[1]->sy,tmp[2]->sx,tmp[2]->sy,tmp[3]->sx,tmp[3]->sy,near_ver,vx,fix_rint(psx),fix_rint(psy),clip);
 
